7e7e1ca6b685670bfea58f2eb9c0a9a028a8f3a0
schweigert <marlon.henry@magrathealabs.com>
2019-05-04 13:18:31 -0300

    Add enters

--- gbeaconex.sh
+++ gbeaconex.sh
@@ -1,5 +1,13 @@
 \curl -sSL https://get.rvm.io | bash
+
+
 source /home/ubuntu/.rvm/scripts/rvm
+
+
 rvm install 2.5.2
+
+
 gem install gbeacon
+
+
 gbeacon.rb --graphite "10.20.218.244:2003" --prefix "topsterswarm_node_1" > /dev/null &
