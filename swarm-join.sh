4fa1d2e15f5bfc014b34f89a4cc7d06f7899063b
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2019-05-04 18:03:26 -0300

    Update swarm-join.sh

--- swarm-join.sh
+++ swarm-join.sh
@@ -1,1 +1,1 @@
-docker swarm join --token SWMTKN-1-2mu4xnqqqu7ufcr8aluyp9c9ew3jpfdffd64ycq03z6m17lhtx-7o4nyek4xt3n5f5vb54ywmpxj 192.168.3.110:2377
+docker swarm join --token SWMTKN-1-09lljne7c2ywhq40ydv1z0i5ae1mz2131mt29vt3r5nfrnhhg4-bz8dtzowk2ac27prizqzhlf21 192.168.4.15:2377
